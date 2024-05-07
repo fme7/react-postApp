10098376e38f8d080750e2b378fc290366593048
fme7 <fatima.mamedova.21@mail.ru>
2024-05-07 15:51:36 -0400

    toggle created

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import Header from "./Header.js";
 import Footer from "./Footer.js";
 import Aside from "./Aside.js";
 import './App.css'
+import { useState } from "react";
 
 function App() {
+  let [menuOpen,setMenuOpen]=useState(true);
+  function toggleMenu(){
+    setMenuOpen(!menuOpen);
+  }
   return (
     <div className="App">
-      <Header></Header>
+      <Header toggle={toggleMenu}></Header>
       <main className="container">
-        <Aside></Aside>
+        <Aside open={menuOpen} ></Aside>
         <div className="routeHolder" >Home</div>
       </main>
       <Footer></Footer>
     </div>
   );
+
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,10 @@
 import "./Header.css";
-function Header() {
+function Header(props){
+   let {toggle} = props 
   return (
     <header className="container">
       <div className="logo">Post App</div>
-      <div className="toggleBtn">
+      <div className="toggleBtn" onClick={toggle} >
         <i className="fa-solid fa-bars"></i>
       </div>
     </header>
